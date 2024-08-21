faa739ccb5ee9acdb9af1a9e6f515f11d260eb77
Cat <an.kyt.ot15@gmail.com>
2024-08-21 22:35:54 +0300

    time calculator program

--- app/src/main/java/com/example/androidlessons/MainActivity.kt
+++ app/src/main/java/com/example/androidlessons/MainActivity.kt
@@ -1,22 +1,27 @@
 package com.example.androidlessons
 
-import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.time.LocalTime
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
-    private lateinit var editText: EditText
-    private lateinit var resultTextView: TextView
-    private lateinit var characterCountTextView: TextView
-    private lateinit var button: Button
+    private lateinit var firstOperandEV: EditText
+    private lateinit var secondOperandEV: EditText
+
+    private lateinit var sumBTN: Button
+    private lateinit var difBTN: Button
+
+    private lateinit var resultTV: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,21 +33,69 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        editText = findViewById(R.id.editTextED)
-        resultTextView = findViewById(R.id.textTV2)
-        characterCountTextView = findViewById(R.id.textTV3)
-        button = findViewById(R.id.buttonBTN)
+        firstOperandEV = findViewById(R.id.firstOperandET)
+        secondOperandEV = findViewById(R.id.secondOperandET)
+        sumBTN = findViewById(R.id.sumBTN)
+        difBTN = findViewById(R.id.difBTN)
+        resultTV = findViewById(R.id.resultRV)
 
-        onButtonClick(button)
+        sumBTN.setOnClickListener(this)
+        difBTN.setOnClickListener(this)
+
+
     }
 
-    fun onButtonClick(view : View){
-        val inputText = editText.text.toString()
-        val reversedText = inputText.reversed()
-        resultTextView.text ="Результат:\n$reversedText"
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onClick(v: View) {
+        val time1 = timeParse(firstOperandEV.text.toString())
+        val time2 = timeParse(secondOperandEV.text.toString())
+        val res = when (v.id) {
+            R.id.sumBTN -> {
+                time1?.plusHours(time2?.hour?.toLong() ?: 0)
+                    ?.plusMinutes(time2?.minute?.toLong() ?: 0)
+                    ?.plusSeconds(time2?.second?.toLong() ?: 0)
+            }
 
-        val characterCount = inputText.replace(" ", "").length
-        characterCountTextView.text = "Количество символов: $characterCount"
+            R.id.difBTN -> {
+                time1?.minusHours(time2?.hour?.toLong() ?: 0)
+                    ?.minusMinutes(time2?.minute?.toLong() ?: 0)
+                    ?.minusSeconds(time2?.second?.toLong() ?: 0)
+            }
+
+            else -> LocalTime.of(0, 0, 0)
+        }
+
+        resultTV.text = formatTime(res)
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun timeParse(input: String): LocalTime? {
+        if (input.isEmpty()) return LocalTime.of(0,0,0)
+        var hours = 0
+        var minutes =0
+        var seconds = 0
+        val matchResult = "(\\d+)(?=h)|(\\d+)(?=m)|(\\d+)(?=s)".toRegex().findAll(input)
+
+        for (match in matchResult) {
+            when {
+                match.groups[1]?.value.takeIf { (it?.toInt() ?: 0) > 0 && (it?.toInt() ?: 0) < 24} != null -> hours = match.groups[1]!!.value.toInt()
+                match.groups[2]?.value.takeIf { (it?.toInt() ?: 0) > 0 && (it?.toInt() ?: 0) < 59} != null -> minutes = match.groups[2]!!.value.toInt()
+                match.groups[3]?.value.takeIf { (it?.toInt() ?: 0) > 0 && (it?.toInt() ?: 0) < 59} != null -> seconds = match.groups[3]!!.value.toInt()
+            }
+        }
+        return LocalTime.of(hours,minutes,seconds)
+
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun formatTime(time: LocalTime?): String {
+        if (time == null) return "0s"
+        return buildString {
+            if (time.hour > 0) append("${time.hour}h")
+            if (time.minute > 0) append("${time.minute}m")
+            if (time.second > 0) append("${time.second}s")
+        }.ifEmpty { "0s" }
+
+    }
 }
