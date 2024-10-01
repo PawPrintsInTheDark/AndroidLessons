167ab4501d68deeb863b86132257c5f41166572c
Cat <an.kyt.ot15@gmail.com>
2024-10-01 20:58:52 +0300

    Checkbox-Snackbar(secondTask)

--- app/src/main/java/com/example/androidlessons/MainActivity.kt
+++ app/src/main/java/com/example/androidlessons/MainActivity.kt
@@ -2,51 +2,44 @@ package com.example.androidlessons
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
+import android.widget.CheckBox
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var inputFieldET: EditText
-    private lateinit var outputFieldTV: TextView
-    private lateinit var saveButton: Button
-    private lateinit var deleteButton: Button
+    private lateinit var textTV : TextView
+    private lateinit var rulesTV : TextView
 
+    lateinit var infoCB : CheckBox
 
-    @SuppressLint("ShowToast")
+    @SuppressLint( "SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        inputFieldET = findViewById(R.id.inputField)
-        outputFieldTV = findViewById(R.id.outputField)
-        saveButton = findViewById(R.id.saveButton)
-        deleteButton = findViewById(R.id.deleteButton)
+        textTV = findViewById(R.id.voidfield)
+        rulesTV = findViewById(R.id.rulesTV)
+        infoCB = findViewById(R.id.infoCB)
 
-        saveButton.setOnClickListener {
-            outputFieldTV.text = inputFieldET.text.toString()
+        infoCB.setOnCheckedChangeListener{_,isChecked ->
+            if (isChecked){
+                textTV.text = "Правила дорожного движения"
+                rulesTV.text =
+                        " 1. Соблюдайте скорость.\n" +
+                        " 2. Используйте ремень безопасности.\n" +
+                        " 3. Не управляйте автомобилем в состоянии алкогольного опьянения.\n" +
+                        " 4. Соблюдайте правила дорожного движения.\n" +
+                        " 5. Уважайте других участников дорожного движения."
+            }else{
+                textTV.text = "Информация"
+                rulesTV.text = ""
+            }
         }
-        deleteButton.setOnClickListener { view ->
-            Snackbar
-                .make(
-                    view,
-                    "Потвердить удаление?",
-                    Snackbar.LENGTH_LONG
-                )
-                .setAction("Удалить?") {
-                    Snackbar.make(
-                        view,
-                        "Данные удалены",
-                        Snackbar.LENGTH_LONG
-                    ).show()
-                    outputFieldTV.text = ""
-                }.show()
-        }
+
+
     }
 
 
