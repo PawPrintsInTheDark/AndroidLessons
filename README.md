98b35f7e523ae3415e8b11f2fd3ad40894f02079
Cat <an.kyt.ot15@gmail.com>
2024-10-02 21:58:08 +0300

    Checkbox-Snackbar(secondTask)

--- README.md
+++ README.md
@@ -4,25 +4,28 @@
 
 ## Скриншоты
 
-## Разметка
+#### 1. На экране приложения необходимо создать заголовок «Электронная книга».
+
+#### 2. Необходимо создать кнопку «Загрузить» для загрузки электронной книги базы данных.
+
 <img src="images/img.png" width="300" />
 
-## Демонстрация работы
+#### 3. В качестве базы данных необходимо создать класс database.kt, в котором будет находиться в переменной text небольшой художественный рассказ.
 
-<img src="images/demo.gif" width="300" />
+<img src="images/img_1.png" width="300" /> 
+
+#### 4. В классе MainActivity при нажатии кнопки загрузки книга загружается из базы данных. Для этого нужно написать функцию loadBook(text: String): List<String>, она возвращает список отдельно взятых слов этой книги.
+
+<img src="images/img_2.png" width="300" />
+
+#### 5. В текстовое поле вывода под кнопкой мы получаем текст книги в результате работы с полученным списком. Т.к. весь текст, вероятно, не поместится в размеры экрана, необходимо чтобы он скролился.
+
+   <img src="images/demo.gif" width="300" /> 
 
 ## Установка
 
 Инструкции по установке проекта:
 
-1. Часть задания:
-
-   ```bash
-   git clone --branch=Checkbox-Snackbar(firstTask) ♦https://github.com/PawPrintsInTheDark/AndroidLessons.git
-   ```
-
-2. Часть задания:
-
-   ```bash
-   git clone --branch=Checkbox-Snackbar(secondTask) ♦https://github.com/PawPrintsInTheDark/AndroidLessons.git
-   ```
+1.
+```bash
+git clone --branch=RelativeLayoutToggleButtonScrollView  https://github.com/PawPrintsInTheDark/AndroidLessons.git
--- app/src/main/java/com/example/androidlessons/MainActivity.kt
+++ app/src/main/java/com/example/androidlessons/MainActivity.kt
@@ -2,44 +2,36 @@ package com.example.androidlessons
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.widget.CheckBox
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var textTV : TextView
-    private lateinit var rulesTV : TextView
+    private lateinit var textTV: TextView
+    private lateinit var downloadBTN: Button
 
-    lateinit var infoCB : CheckBox
-
-    @SuppressLint( "SetTextI18n")
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        textTV = findViewById(R.id.voidfield)
-        rulesTV = findViewById(R.id.rulesTV)
-        infoCB = findViewById(R.id.infoCB)
+        downloadBTN = findViewById(R.id.downloadButton)
+        textTV = findViewById(R.id.textView)
 
-        infoCB.setOnCheckedChangeListener{_,isChecked ->
-            if (isChecked){
-                textTV.text = "Правила дорожного движения"
-                rulesTV.text =
-                        " 1. Соблюдайте скорость.\n" +
-                        " 2. Используйте ремень безопасности.\n" +
-                        " 3. Не управляйте автомобилем в состоянии алкогольного опьянения.\n" +
-                        " 4. Соблюдайте правила дорожного движения.\n" +
-                        " 5. Уважайте других участников дорожного движения."
-            }else{
-                textTV.text = "Информация"
-                rulesTV.text = ""
+        downloadBTN.setOnClickListener{
+
+            for (w in loadBook(Database().text)){
+                textTV.append(w + "\n")
             }
         }
+    }
 
 
+    private fun loadBook(text: String): List<String> {
+        return text.split(" ")
     }
 
 
