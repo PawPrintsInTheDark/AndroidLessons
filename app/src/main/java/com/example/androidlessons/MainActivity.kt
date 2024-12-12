5ea2f8f1908d6aa7b50574e904b6350ccfb11ef1
Cat <an.kyt.ot15@gmail.com>
2024-12-12 23:10:58 +0500

    AutoMultiCompleteTextView

--- app/src/main/java/com/example/androidlessons/MainActivity.kt
+++ app/src/main/java/com/example/androidlessons/MainActivity.kt
@@ -1,94 +1,136 @@
 package com.example.androidlessons
 
-import android.annotation.SuppressLint
-import android.app.AlarmManager
-import android.app.AlarmManager.RTC_WAKEUP
-import android.app.PendingIntent
-import android.content.Intent
-import android.os.Build
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Button
-import android.widget.TextView
-import androidx.annotation.RequiresApi
+import android.widget.EditText
+import android.widget.ListView
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatDelegate
-import com.google.android.material.timepicker.MaterialTimePicker
-import com.google.android.material.timepicker.TimeFormat
-import java.text.SimpleDateFormat
-import java.util.Calendar
-import java.util.Locale
 
+@Suppress(" CAST_NEVER_SUCCEEDS")
 class MainActivity : AppCompatActivity() {
 
-    private var calendar: Calendar? = null
-    private var materialTimePicker: MaterialTimePicker? = null
+    private var role = mutableListOf(
+        "Должность",
+        "Разработчик",
+        "Старший разработчик",
+        "Технический лидер",
+        "Архитектор ПО",
+        "Тестировщик",
+        "Аналитик данных",
+        "DevOps-инженер",
+        "Системный администратор",
+        "UI/UX-дизайнер",
+        "Продуктовый менеджер",
+        "Маркетолог в IT",
+        "Специалист по кибербезопасности",
+        "Технический писатель",
+        "Стажер-программист"
+    )
 
-    private lateinit var setAlarmTV: TextView
-    private lateinit var alarmButtonBTN: Button
+    private var persons: MutableList<Person> = mutableListOf()
+    private var listAdapter: ListAdapter? = null
 
+
+    private lateinit var postSpinner: Spinner
+    private lateinit var ageET: EditText
+    private lateinit var nameET: EditText
+    private lateinit var secondNameET: EditText
+    private lateinit var addBTN: Button
+    private lateinit var listViewLV: ListView
 
     private lateinit var toolbar: androidx.appcompat.widget.Toolbar
 
+    var post: String = ""
 
-    @SuppressLint("SetTextI18n")
-    @RequiresApi(Build.VERSION_CODES.O)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val dateFormat = SimpleDateFormat("HH:mm", Locale.getDefault())
-
-
         init()
-        toolbar.title = "Будильник"
+
+        toolbar.title = "Подбор персонала"
         setSupportActionBar(toolbar)
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO) // для отображения светлой темы
 
-        alarmButtonBTN.setOnClickListener {
-            materialTimePicker = MaterialTimePicker.Builder()
-                .setTimeFormat(TimeFormat.CLOCK_24H)
-                .setHour(12)
-                .setMinute(0)
-                .setTitleText("Выберите время будильника")
-                .build()
-            materialTimePicker!!.addOnPositiveButtonClickListener {
-                calendar = Calendar.getInstance()
-                calendar?.set(Calendar.SECOND, 0)
-                calendar?.set(Calendar.MILLISECOND, 0)
-                calendar?.set(Calendar.MINUTE, materialTimePicker!!.minute)
-                calendar?.set(Calendar.HOUR, materialTimePicker!!.hour)
+        configureSpinner()
 
-                val alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
+        addBTN.setOnClickListener {
+            createProduct()
+            listAdapter = ListAdapter(this@MainActivity, persons)
+            listViewLV.adapter = listAdapter
+            listAdapter!!.notifyDataSetChanged()
 
-                alarmManager.setExact(
-                    RTC_WAKEUP,
-                    calendar?.timeInMillis!!,
-                    getAlarmPendingIntent()!!
-                )
-                setAlarmTV.text = "Будильник установлен на ${dateFormat.format(calendar!!.time)}"
-            }
-            materialTimePicker!!.show(supportFragmentManager, "tag_picker")
+            resetEditFields()
+
         }
 
-    }
+        listViewLV.onItemClickListener =
+            AdapterView.OnItemClickListener { _, _, position, _ ->
+//                val product = listViewLV.adapter.getItem(position) as? Person
+                persons.removeAt(position)
+                listAdapter?.notifyDataSetChanged()
+            }
 
-    private fun getAlarmPendingIntent(): PendingIntent? {
-        val intent = Intent(this, AlarmReceiver::class.java)
-        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
-        return PendingIntent.getBroadcast(
-            this,
-            1,
-            intent,
-            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
-        )
     }
 
     private fun init() {
         toolbar = findViewById(R.id.toolbarMain)
-        alarmButtonBTN = findViewById(R.id.alertButtonBTN)
-        setAlarmTV = findViewById(R.id.setAlarmTV)
+        postSpinner = findViewById(R.id.postSpinner)
+        ageET = findViewById(R.id.ageET)
+        nameET = findViewById(R.id.nameET)
+        secondNameET = findViewById(R.id.secondNameET)
+        addBTN = findViewById(R.id.addBTN)
+        listViewLV = findViewById(R.id.listViewLV)
+    }
+
+    private fun resetEditFields() {
+        nameET.text.clear()
+        secondNameET.text.clear()
+        ageET.text.clear()
+        postSpinner.setSelection(0)
+    }
+
+    private fun createProduct() {
+        persons.add(
+            Person(
+                nameET.text.toString(),
+                secondNameET.text.toString(),
+                ageET.text.toString(),
+                post
+            )
+        )
+    }
+
+    private fun configureSpinner() {
+        val adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_item,
+            role
+        )
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        postSpinner.adapter = adapter
+
+        val itemSelectedListener: AdapterView.OnItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?, view: View?, position: Int, id: Long
+                ) {
+                    val item = parent?.getItemAtPosition(position) as String
+                    post = item
+
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                }
+            }
+        postSpinner.onItemSelectedListener = itemSelectedListener
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
---
+++ app/src/main/java/com/example/androidlessons/Person.kt
@@ -0,0 +1,8 @@
+package com.example.androidlessons
+
+data class Person(
+    val name: String,
+    val secondName: String,
+    val age: String,
+    val post: String
+)
