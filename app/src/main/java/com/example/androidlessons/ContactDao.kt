efd2725c26b19461b0cc326ebbaa8435d55ef4c4
Cat <an.kyt.ot15@gmail.com>
2024-12-22 20:12:42 +0500

    RoomPracticeNote

--- app/src/main/java/com/example/androidlessons/ContactDao.kt
+++ app/src/main/java/com/example/androidlessons/ContactDao.kt
@@ -1,5 +1,6 @@
 package com.example.androidlessons
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
@@ -14,7 +15,7 @@ interface ContactDao {
     suspend fun delete(contact: Contact)
 
     @Query("SELECT * FROM contacts_table ORDER BY id ASC")
-    suspend fun getAllContacts(): List<Contact>
+    fun getAllContacts(): LiveData<List<Contact>>
 
     @Query("DELETE FROM contacts_table")
     suspend fun deleteAll()
--- app/src/main/java/com/example/androidlessons/MainActivity.kt
+++ app/src/main/java/com/example/androidlessons/MainActivity.kt
@@ -4,22 +4,29 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.launch
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import java.text.SimpleDateFormat
 import java.util.Date
+import java.util.SimpleTimeZone
+import java.util.TimeZone
 
-class MainActivity : AppCompatActivity() {
-    var db: ContactDatabase? = null
+class MainActivity : AppCompatActivity() , ContactAdapter.ContactClickListener {
+    private lateinit var viewModel: ContactViewModel
+
     private lateinit var toolbar: Toolbar
     private lateinit var surnameET: EditText
     private lateinit var phoneET: EditText
     private lateinit var saveBTN: Button
-    private lateinit var textView: TextView
+    private lateinit var recyclerView: RecyclerView
 
     @SuppressLint("CommitTransaction")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,40 +39,44 @@ class MainActivity : AppCompatActivity() {
         surnameET = findViewById(R.id.surnameET)
         phoneET = findViewById(R.id.phoneET)
         saveBTN = findViewById(R.id.saveBTN)
-        textView = findViewById(R.id.textView)
+        recyclerView = findViewById(R.id.recyclerView)
 
-        db = ContactDatabase.getDatabase(this)
-        readDatabase()
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        val adapter = ContactAdapter(this,this)
+        recyclerView.adapter = adapter
+
+        viewModel = ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory
+            .getInstance(application))[ContactViewModel::class.java]
+        viewModel.contacts.observe(this, Observer { list ->
+            list?.let {
+                adapter.updateList(it)
+            }
+        })
+
     }
 
-    override fun onResume() {
-        super.onResume()
-        saveBTN.setOnClickListener {
-            val contact = Contact(surnameET.text.toString(), phoneET.text.toString(), Date().time.toString())
-            addContact(contact)
-            readDatabase()
-//            lifecycleScope.launch{
-//                db?.getContactDao()?.deleteAll()
-//            }
+    override fun onItemClicked(contact: Contact) {
+        viewModel.deleteContact(contact)
+        Toast.makeText(this, "Контакт ${contact.surname} удалён.", Toast.LENGTH_SHORT).show()
+    }
+
+    fun saveData(view:View){
+        val contactSurname = surnameET.text.toString()
+        val  contactPhone = phoneET.text.toString()
+        val timestamp = formatMilliseconds(Date().time)
+        if (contactPhone.isNotEmpty() || contactSurname.isNotEmpty()){
+            viewModel.insertContact(Contact(contactSurname,contactPhone, timestamp))
+            Toast.makeText(this, "Данные сохранены", Toast.LENGTH_SHORT).show()
             surnameET.text.clear()
             phoneET.text.clear()
         }
     }
 
-    private fun addContact(contact: Contact) {
-        lifecycleScope.launch {
-            db?.getContactDao()?.insert(contact)
-        }
-    }
-
-    private fun readDatabase() {
-        lifecycleScope.launch {
-            textView.text = ""
-            val list = db?.getContactDao()?.getAllContacts() ?: emptyList()
-            list.forEach { i ->
-                textView.append("Фамилия: ${i.surname}, Тел. ${i.phone}\n")
-            }
-        }
+    private fun formatMilliseconds(time: Long): String {
+        val timeFormat = SimpleDateFormat("EEE, HH:mm")
+        timeFormat.timeZone = TimeZone.getTimeZone("GMT+04")
+        return timeFormat.format(Date(time))
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -75,6 +86,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         finishAffinity()
-        return super.onOptionsItemSelected(item)
+        return true
     }
 }
