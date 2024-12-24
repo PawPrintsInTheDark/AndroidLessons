12bbc3f1093635735cd4d18846d557f48fb2ca4d
Cat <an.kyt.ot15@gmail.com>
2024-12-24 23:24:25 +0500

    VideoView

--- app/src/main/java/com/example/androidlessons/MainActivity.kt
+++ app/src/main/java/com/example/androidlessons/MainActivity.kt
@@ -1,13 +1,8 @@
 package com.example.androidlessons
 
-import android.animation.ObjectAnimator
-import android.animation.ValueAnimator
-import android.graphics.drawable.AnimationDrawable
-import android.media.MediaPlayer
+import android.net.Uri
 import android.os.Bundle
-import android.os.Handler
-import android.view.animation.LinearInterpolator
-import android.widget.SeekBar
+import android.widget.MediaController
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import com.example.androidlessons.databinding.ActivityMainBinding
@@ -15,140 +10,48 @@ import com.example.androidlessons.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private var mediaPlayer: MediaPlayer? = null
-    private val songList = listOf(R.raw.cyberworld, R.raw.alchemy, R.raw.vitality)
-    private var currentSong = 0
-    private lateinit var animator: ValueAnimator
-    private val handler = Handler()
+    private var currentVideo = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setBackground()
-        setupMediaPlayer()
-        setupAnimation()
-    }
 
-    private fun setupMediaPlayer() {
-        binding.playFAB.setOnClickListener { playSong() }
+        val videoList = mutableListOf(
+            Uri.parse("android.resource://$packageName/${R.raw.video1}"),
+            Uri.parse("android.resource://$packageName/${R.raw.video2}"),
+            Uri.parse("android.resource://$packageName/${R.raw.video3}"),
+        )
+
+        val mediaController = MediaController(this)
+        mediaController.setAnchorView(binding.videoView)
+        binding.videoView.setMediaController(mediaController)
+
+        playVideo(videoList[currentVideo])
+
+        binding.previosFAB.setOnClickListener {
+            currentVideo = (currentVideo - 1 + videoList.size) % videoList.size
+            playVideo(videoList[currentVideo])
+        }
+
         binding.pauseFAB.setOnClickListener {
-            mediaPlayer?.pause()
-            stopAnimationDisc()
-        }
-        binding.stopFAB.setOnClickListener { stopSong() }
-        binding.nextFAB.setOnClickListener { nextSong() }
-        binding.previousFAB.setOnClickListener { previousSong() }
-        binding.seekbarSB.setOnSeekBarChangeListener(createSeekBarChangeListener())
-        binding.seekbarVolumeSB.setOnSeekBarChangeListener(createSeekBarVolumeChangeListener())
-    }
-
-    private fun setupAnimation() {
-        animator = ValueAnimator.ofFloat(0f, 360f)
-        animator.duration = 2500
-        animator.interpolator = LinearInterpolator()
-        animator.repeatCount = ObjectAnimator.INFINITE
-        animator.addUpdateListener { animation ->
-            val rotationValue = animation.animatedValue as Float
-            binding.imageViewIV.rotation = rotationValue
-        }
-    }
-
-    private fun createSeekBarVolumeChangeListener() = object : SeekBar.OnSeekBarChangeListener {
-        override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-            if (fromUser) {
-                val volume = progress / 100f
-                mediaPlayer?.setVolume(volume, volume)
+            if (binding.videoView.isPlaying) {
+                binding.videoView.pause()
+            } else {
+                binding.videoView.start()
             }
         }
 
-        override fun onStartTrackingTouch(seekBar: SeekBar?) {}
-        override fun onStopTrackingTouch(seekBar: SeekBar?) {}
-    }
-
-    private fun playSong() {
-        if (mediaPlayer == null) {
-            mediaPlayer = MediaPlayer.create(this, songList[currentSong])
-            initializeSeekBar()
-        }
-        mediaPlayer?.start()
-        playAnimationDisc()
-    }
-
-    private fun stopSong() {
-        mediaPlayer?.apply {
-            stop()
-            reset()
-            release()
-        }
-        mediaPlayer = null
-        stopAnimationDisc()
-    }
-
-    private fun nextSong() {
-        currentSong = (currentSong + 1) % songList.size
-        resetMediaPlayer()
-    }
-
-    private fun previousSong() {
-        currentSong = (currentSong - 1 + songList.size) % songList.size
-        resetMediaPlayer()
-    }
-
-    private fun resetMediaPlayer() {
-        mediaPlayer?.apply {
-            stop()
-            reset()
-            release()
-        }
-        mediaPlayer = MediaPlayer.create(this, songList[currentSong])
-        mediaPlayer?.start()
-        playAnimationDisc()
-    }
-
-    private fun createSeekBarChangeListener() = object : SeekBar.OnSeekBarChangeListener {
-        override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-            if (fromUser) mediaPlayer?.seekTo(progress)
-        }
-
-        override fun onStartTrackingTouch(seekBar: SeekBar?) {}
-        override fun onStopTrackingTouch(seekBar: SeekBar?) {}
-    }
-
-    private fun initializeSeekBar() {
-        binding.seekbarVolumeSB.max = 100
-
-        binding.seekbarSB.max = mediaPlayer!!.duration
-        handler.postDelayed(object : Runnable {
-            override fun run() {
-                mediaPlayer?.let {
-                    binding.seekbarSB.progress = it.currentPosition
-                    handler.postDelayed(this, 1000)
-                }
-            }
-        }, 0)
-    }
-
-    private fun setBackground() {
-        val animation = binding.mainLayout.background as AnimationDrawable
-        animation.apply {
-            setEnterFadeDuration(500)
-            setExitFadeDuration(1500)
-            start()
+        binding.nextFAB.setOnClickListener {
+            currentVideo = (currentVideo + 1) % videoList.size
+            playVideo(videoList[currentVideo])
         }
     }
 
-    private fun playAnimationDisc() {
-        if (!animator.isRunning){
-            animator.start()
-        }else if (animator.isPaused){
-            animator.start()
-        }
-    }
-
-    private fun stopAnimationDisc() {
-        animator.pause()
+    private fun playVideo(uri: Uri) {
+        binding.videoView.setVideoURI(uri)
+        binding.videoView.requestFocus()
+        binding.videoView.start()
     }
 }
-
