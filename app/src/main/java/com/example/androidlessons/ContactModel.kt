dd3a6ebd11c27ba27082a38c1eef2e5ab8ce2419
Cat <an.kyt.ot15@gmail.com>
2024-12-25 23:16:31 +0500

    ContentProviderWriteContact

--- app/src/main/java/com/example/androidlessons/ContactModel.kt
+++ app/src/main/java/com/example/androidlessons/ContactModel.kt
@@ -1,6 +1,8 @@
 package com.example.androidlessons
 
+import java.io.Serializable
+
 data class ContactModel(
     val name: String,
     val phone: String
-)
+):Serializable
--- app/src/main/java/com/example/androidlessons/MainActivity.kt
+++ app/src/main/java/com/example/androidlessons/MainActivity.kt
@@ -2,12 +2,17 @@ package com.example.androidlessons
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.content.ContentProviderOperation
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.drawable.AnimationDrawable
 import android.net.Uri
 import android.os.Bundle
 import android.provider.ContactsContract
+import android.provider.ContactsContract.CommonDataKinds.Phone
+import android.provider.ContactsContract.CommonDataKinds.StructuredName
+import android.provider.ContactsContract.RawContacts
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
@@ -53,21 +58,95 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+
+    @SuppressLint("NotifyDataSetChanged")
+    override fun onResume() {
+        super.onResume()
+        binding.addContactButton.setOnClickListener {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.WRITE_CONTACTS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                permissionWriteContact.launch(Manifest.permission.WRITE_CONTACTS)
+            } else {
+                addContact()
+                getContact()
+            }
+        }
+    }
+
+
+    private fun addContact() {
+        val newContactName = binding.nameInput.text.toString()
+        val newContactPhone = binding.phoneInput.text.toString()
+
+        if (newContactName.isEmpty() || newContactPhone.isEmpty()) {
+            Toast.makeText(this, "Имя и номер телефона не могут быть пустыми", Toast.LENGTH_SHORT)
+                .show()
+            return
+        }
+
+        val listCPO = ArrayList<ContentProviderOperation>()
+
+        listCPO.add(
+            ContentProviderOperation.newInsert(RawContacts.CONTENT_URI)
+                .withValue(RawContacts.ACCOUNT_TYPE, null)
+                .withValue(RawContacts.ACCOUNT_NAME, null)
+                .build()
+        )
+        listCPO.add(
+            ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
+                .withValue(ContactsContract.Data.MIMETYPE, StructuredName.CONTENT_ITEM_TYPE)
+                .withValue(StructuredName.DISPLAY_NAME, newContactName)
+                .build()
+        )
+        listCPO.add(
+            ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
+                .withValue(ContactsContract.Data.MIMETYPE, Phone.CONTENT_ITEM_TYPE)
+                .withValue(Phone.NUMBER, newContactPhone)
+                .withValue(Phone.TYPE, Phone.TYPE_MOBILE)
+                .build()
+        )
+
+        try {
+            contentResolver.applyBatch(ContactsContract.AUTHORITY, listCPO)
+            Toast.makeText(this, "$newContactName добавлен в список контактов", Toast.LENGTH_SHORT)
+                .show()
+        } catch (e: Exception) {
+            Log.e("Exception", e.message ?: "Ошибка при добавлении контакта")
+        }
+    }
+
+    private val permissionWriteContact = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted) {
+            Toast.makeText(this, "Получен доступ к записи контактов", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "В разрешении отказано...", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
     @SuppressLint("Range")
     private fun getContact() {
+        contactModelList.clear()
         val phoneCursor = contentResolver.query(
-            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+            Phone.CONTENT_URI,
             null,
             null,
             null,
-            ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " ASC"
+            Phone.DISPLAY_NAME + " ASC"
         )
 
         while (phoneCursor!!.moveToNext()) {
             val name =
-                phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME))
+                phoneCursor.getString(phoneCursor.getColumnIndex(Phone.DISPLAY_NAME))
             val phoneNumber =
-                phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
+                phoneCursor.getString(phoneCursor.getColumnIndex(Phone.NUMBER))
             val contactModel = ContactModel(name, phoneNumber)
             contactModelList.add(contactModel)
         }
@@ -122,12 +201,25 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.main_menu, menu)
+        menuInflater.inflate(R.menu.search_menu, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        finishAffinity()
-        return false
+        when (item.itemId) {
+            R.id.exitMenu -> finishAffinity()
+            R.id.searchMenu -> {
+                val intent = Intent(this, SearchActivity::class.java)
+                intent.putExtra("CONTACT_LIST", ArrayList(contactModelList))
+                startActivity(intent)
+                return true
+            }
+
+            else -> return super.onOptionsItemSelected(item)
+        }
+        return true
     }
+
+
 }
+
